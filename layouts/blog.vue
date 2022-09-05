a3264f7e600d9866bdfb69ac63baf53f553c2991
davidyoon <qqpo12@naver.com>
2022-09-05 21:21:07 +0900

    Add layouts
    * Added layouts/blog.vue
    * Applied a blog layout

---
+++ layouts/blog.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div>
+        My blog navigation bar here
+        </div>
+        <Nuxt />
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
--- pages/board/index.vue
+++ pages/board/index.vue
@@ -5,9 +5,16 @@
 </template>
 <script>
 export default {
-    
+    layout: 'blog'
 }
 </script>
 <style>
-    
+.container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+}
 </style>
